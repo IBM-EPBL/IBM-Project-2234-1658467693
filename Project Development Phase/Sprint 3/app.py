a18a3109d8241b5b47f4ef2e788cb5f97c2a6cd8
DharaniSri <99311313+DharaniSrii@users.noreply.github.com>
2022-11-18 14:30:37 +0530

    Update app.py

--- Project Development Phase/Sprint 3/app.py
+++ Project Development Phase/Sprint 3/app.py
@@ -14,7 +14,6 @@ app = Flask(__name__)
 
 #Email
 app.config['SECRET_KEY'] = 'top-secret!'
-app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'apikey'
